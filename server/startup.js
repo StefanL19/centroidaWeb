5e444323f9c6ce8259edc1d31250baf7df4b7c6d
Hristo Georgiev <hgeorgievkzr@gmail.com>
2016-01-04 12:23:26 +0200

    admin configured

--- server/startup.js
+++ server/startup.js
@@ -1,4 +1,14 @@
 Meteor.startup(function () {
+
+if(Meteor.users.find().count() == 0) {
+ var id = Accounts.createUser({
+    email: "admin@theia.bg",
+    password: "123123123",
+    profile: { name: "Admin"}
+  });
+
+ Roles.addUsersToRoles(id, ['admin']);
+}
 if (Products.find().count() == 0) {
     Products.insert({
         name : "Fustuchen Tahan",

7c3162064e027417cec2b27c2ce5b59a809aecb3
StefanL19 <stlazov@abv.bg>
2016-01-13 14:30:46 +0100

    added href attributes

--- client/website.js
+++ client/website.js
@@ -2,71 +2,31 @@
 Meteor.subscribe("recipes");
 Meteor.subscribe("products");
 
-//PRODDUCTS
-Template.products.helpers({
-    product: function () {
-        return Products.find({});
-    },
-});
 
 
-Template.singleProduct.helpers({
-    product:function(){
-        var currentId = Session.get('prodId');
-        var product = Products.findOne({_id:currentId});
-        Session.set('productName', product.title);
-        return product;
-    }
-});
-
-
-Template.singleProduct.events({
-
-    'click .modal_activate':function(event){
-        console.log("clicked modal activate");
-        $("#productOrder").modal('show');
-    },
-
-    'submit .js-add-order':function(event){
-        var email = event.target.email.value;
-        var name = event.target.name.value;
-        var familyName = event.target.family_name.value;
-        var adress = event.target.adress.value;
-        var quantity = event.target.quantity.value;
-        var productName = Session.get('productName');
-        var emailText = "Име: "+name +" "+familyName + "\r\n" + "Адрес: " + adress+ "\r\n" +
-                          "Поръчка: " + quantity + "\r\n" + "Стока: " + productName + "\r\n" + "Email: " + email;
-        var mailSubject = "Поръчка";
-        console.log(emailText);
-
-
-        Meteor.call('sendEmail',
-            'testemail',
-            'website',
-          mailSubject,
-            emailText);
-
-        return false;
-    }
-
-});
-
-
-//RECIPES
-Template.recipes.helpers({
+//METEOR ALL POSTS HELPERS+EVENTS
+Template.meteorpost.helpers({
     current_theme: function () {
-      console.log(Blog.Post.find({tags:"meteor"}));
         return Blog.Post.find({tags:"meteor"});
     },
+
 });
 
+Template.meteorpost.events({
+  'click .js-meteor-blogpost':function(event){
+    var p = this.slug;
+    $(".linkMeteorPost").attr("href", "/blog/"+p);
+      
+  }
+})
 
 
-Template.singleRecipe.helpers({
-    recipe:function(){
-        var currentId = Session.get('recId');
-        var recipe = Recipes.findOne({_id:currentId});
-        return recipe;
+
+Template.singlePost.helpers({
+    post:function(){
+        var currentId = Session.get('meteorId');
+        var meteorpost = Blog.Post.find({_id:currentId});
+        return meteorpost.fetch({})[0];
     }
 });
 
@@ -78,7 +38,6 @@ Template.contact.helpers({
 })
 Template.contact.events({
    'submit .js-send-message': function(event) {
-      console.log(Blog.posts.find({}));
        event.preventDefault();
 
        if(validateContactMail()) {
